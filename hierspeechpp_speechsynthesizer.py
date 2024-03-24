493b800a8715a9141888da7702d503eb3e100813
nikravan <nikravaninform@gmail.com>
2024-03-24 06:32:16 -0700

    Update hierspeechpp_speechsynthesizer.py
    chage cuda to to(device)

--- hierspeechpp_speechsynthesizer.py
+++ hierspeechpp_speechsynthesizer.py
@@ -18,6 +18,8 @@ import commons
 from alias_free_torch import *
 import activations
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 class Wav2vec2(torch.nn.Module):
     def __init__(self, layer=7, w2v='mms'):
 
@@ -629,7 +631,7 @@ class SynthesizerTrn(nn.Module):
 
       self.emb = torch.nn.Embedding(1, 256)
       torch.nn.init.normal_(self.emb.weight, 0.0, 256 ** -0.5)
-      self.null = torch.LongTensor([0]).cuda()
+      self.null = torch.LongTensor([0]).to(device)
       self.uncond_ratio = uncond_ratio
     self.cfg = cfg
   @torch.no_grad()
